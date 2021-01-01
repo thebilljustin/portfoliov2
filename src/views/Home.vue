7edde3bbb20b77b674679f68e4b8b1b7e2b52833
cheb <bill.manalo@springvalley.tech>
2021-01-01 23:33:27 +0900

    complete landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="banner">
+		<div class="content">
+			<div class="circle"></div>
+			<h1>Hi. I'm <span>Bill Justin</span> <br /> I create professional websites.</h1>
+			<div class="links">
+				<a href="#" data-text="about">About Me</a>
+				 <a href="#" data-text="contact">Contact</a>
+				<a href="#" data-text="portfolio">Portfolio</a>
+				
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import VanillaTilt from 'vanilla-tilt'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+	data() 
+	{
+		return {
+
+		}
+	},
+
+	mounted()
+	{
+		VanillaTilt.init(document.querySelector(".content"), {
+			max: 25,
+			speed: 1000,
+			// reverse: true
+		});
+	}
 }
 </script>
+
+<style>
+	body {
+		overflow-y: hidden; 
+	}
+</style>
